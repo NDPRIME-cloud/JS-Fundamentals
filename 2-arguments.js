0627eb54b1b0dd2daa4bb4577bbc5f83fb09ed06
ndprime <nikechideradivine@gmail.com>
2025-07-11 01:08:24 +0100

    Arguments

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,8 +1,8 @@
-const args= Process.argv.slice(2);
+const args= process.argv.slice(2);
 if (args.length ===0){
     console.log("No argument")
 }else if(args.length ===1){
-    console.log("Best Argument found")
+    console.log("Argument found")
 }else{
-    console.log ("Best school Argument found")
+    console.log ("Argument found")
 }
