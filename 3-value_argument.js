ee7fa15a74c4aa768bada1cd20702ac8078a7b2c
ndprime <nikechideradivine@gmail.com>
2025-07-11 01:30:04 +0100

    value of my argument

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -2,9 +2,9 @@ const args = process.argv.slice(2)
 if(!args[0]){
     console.log ("No argument")
 }
-if(!args[0]){
+if(!args[1]){
     console.log("HBTN")
 }
-if(!args[0]){
+if(!args[2]){
     console.log("HBTN cool")
 }
