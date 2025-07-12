2dc1d9776ac0b25d1e10e055663b4398ba53c7f2
ndprime <nikechideradivine@gmail.com>
2025-07-12 09:52:09 +0100

    done

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,10 +1,6 @@
 const args = process.argv.slice(2)
 if(!args[0]){
     console.log ("No argument")
-}
-if(!args[1]){
-    console.log("HBTN")
-}
-if(!args[2]){
-    console.log("HBTN cool")
+} else {
+    console.log(args[0])
 }
