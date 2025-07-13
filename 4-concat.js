1b2005299515f8e2c1021c4be238f124d2901300
ndprime <nikechideradivine@gmail.com>
2025-07-13 19:20:24 +0100

    done

--- 4-concat.js
+++ 4-concat.js
@@ -1,4 +1,4 @@
 const args = process.agrv.slice(2)
-console.log(args[0] + "is" + args[2])
+console.log(args[0] + "is" + args[1])
 console.log(args[0] + "is" + args[4])
 console.log(args[10] + "is" + args[11] )
