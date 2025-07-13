98e542c597be1de81fed89178dfe89056246aef1
ndprime <nikechideradivine@gmail.com>
2025-07-13 20:33:13 +0100

    done

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,4 +1,4 @@
-let myArray = ["C is fun", "Python is cool", "Javascriptis amazing"]
-     for(i = 0; i< myArray.length; i++){
+let myArray = ["C is fun", "Python is cool", "Javascript is amazing"]
+     for(let i = 0; i < myArray.length; i++){
         console.log(myArray[i])
      }
