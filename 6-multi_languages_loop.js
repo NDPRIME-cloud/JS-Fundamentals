5889c305543a34899414d8b32c30a1ead44f6ad9
ndprime <nikechideradivine@gmail.com>
2025-07-13 20:54:58 +0100

    done

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,5 +1,6 @@
 const lines = ["C is fun", "Python is cool", "Javascript is amazing"]
-     for(let i = 0; i < lines.length; i++){
+let i = 0;
+while( i < lines.length ){
         console.log(lines[i]);
-     }
+   i++  }
     
