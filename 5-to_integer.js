3d8e8add218944568b50e9a4935bb9baaea3af8f
ndprime <nikechideradivine@gmail.com>
2025-07-13 19:56:59 +0100

    done

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,6 +1,6 @@
 const args = process.argv.slice(2)
 let myNumber = parseInt(args)
-if(myNumber!==Number){
+if(isNaN(myNumber)){
     console.log("Not a number")
 }else{
   console.log("My number: " + myNumber)  }
