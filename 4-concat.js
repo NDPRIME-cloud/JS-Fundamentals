6d4437ec9ccb817aa9a75dfb580c29c6539289c8
ndprime <nikechideradivine@gmail.com>
2025-07-13 19:58:25 +0100

    done

--- 4-concat.js
+++ 4-concat.js
@@ -1,7 +1,4 @@
 const args = process.argv.slice(2);
-// console.log(`${args[0]} is ${args[1]}`);
-// console.log(`${args[0]} is ${args[9]}`);
-// console.log(`${args[6]} is ${args[11]}`);
 console.log(args[0]  + " is "  + args[1]);
 console.log(args[0] +  " is "  + args[9]);
 console.log(args[10] +  " is "  + args[11]);
